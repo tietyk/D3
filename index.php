f2ca8dc880c9764de026d13ba285da515068915f
Tiety <tietyk@hotmail.com>
2015-04-20 10:24:20 +0200

    test index

---
+++ index.php
@@ -0,0 +1,18 @@
+<<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<title></title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<h1>test</h1>
+		<p>
+			
+		</p>
+
+	</body>
+</html>
